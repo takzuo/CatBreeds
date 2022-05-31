554be671174ec66d48fd22feafa0cc0552d73891
JulianArango <julian.arango@aituring.co>
2022-05-31 02:01:03 -0500

    refactor: TT5-Connections-api: Delete test files

--- lib/domain/repositories/authentication_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:catbreeds/domain/responses/response.dart';
-
-abstract class AuthenticationRepository{
-  Future<String?> get accessToken;
-  Future<Response> login(String email, String password);
-}
